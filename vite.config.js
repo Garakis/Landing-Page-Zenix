ce6b7706e91d93ae988e9e385dd257872b5961f6
Emmanuel Garakis <emmanogarakis@gmail.com>
2026-03-04 13:20:04 -0300

    feat: Add responsive Navbar component and configure Vite base path for deployment.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Landing-Page-Zenix/',
   plugins: [
     react(),
     tailwindcss(),
